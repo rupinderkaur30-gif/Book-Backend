872747aa259e82bbc439f8ccc5875b0c4c175b5d
rupinderkaur <rupi063094@gmail.com>
2023-12-03 01:14:51 -0500

    postanddeleterequest

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,7 +1,24 @@
 class BooksController < ApplicationController
-
+  
     def index 
         books = Book.all
         render json: books
     end 
+
+    def create 
+        book = Book.create(book_params)
+        render json: book
+    end
+
+    def destroy
+        book = Book.find(params[:id])
+        book.destroy
+        render json: {}
+    end
+
+    private 
+
+    def book_params
+        params.require(:book).permit(:title, :author, :genre, :years_published, :image_url)
+    end
 end
